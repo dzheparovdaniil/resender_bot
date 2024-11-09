7921725f8bd0014479458a5efda061f529791752
dzheparovdaniil <daniil.dzheparov@yandex.ru>
2024-11-09 13:48:04 +0300

    добавил публикацию в инсту

--- handlers/channel_messages.py
+++ handlers/channel_messages.py
@@ -1,6 +1,6 @@
 from aiogram import types, F, Router
 from bot import bot
-from keyboard import post_keyboard
+from keyboard import post_keyboard, post_keyboard_inst
 
 router_channel = Router()
 
@@ -8,11 +8,10 @@ photos = {}
 
 
 async def forward_channel_messages(message: types.Message):
-    print('работает функция forward_channel_messages')
     if message.text:
         await bot.send_message(chat_id=874188918, text=message.html_text, parse_mode='HTML', reply_markup=post_keyboard)
     elif message.photo and message.media_group_id == None:
-        await bot.send_photo(chat_id=874188918, photo=message.photo[-1].file_id, caption=message.html_text, parse_mode='HTML', reply_markup=post_keyboard)
+        await bot.send_photo(chat_id=874188918, photo=message.photo[-1].file_id, caption=message.html_text, parse_mode='HTML', reply_markup=post_keyboard_inst)
     elif message.photo and message.media_group_id != None:
         global photos
         key = str(message.media_group_id)
@@ -21,7 +20,6 @@ async def forward_channel_messages(message: types.Message):
 
 @router_channel.message(F.media_group_id == None)
 async def send_echo(message: types.Message):
-    print('работает функция send_echo')
     if message.photo:
         await message.answer_photo(photo=message.photo[-1].file_id, caption = message.caption, parse_mode='HTML', reply_markup=post_keyboard)
     else:
